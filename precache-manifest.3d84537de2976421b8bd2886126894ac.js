953f7ff32ebb147f88d7069365544e34041d4d77
hadisMansori <hadis.mans97@gmail.com>
2019-01-30 04:14:49 +0330

    Updates

---
+++ precache-manifest.3d84537de2976421b8bd2886126894ac.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "f805f61e64c7ee471453c544c1818c17",
+    "url": "/hospital/static/media/IRHoma_p30download.com.f805f61e.ttf"
+  },
+  {
+    "revision": "2b4d64d2e4f1a60ed1f6",
+    "url": "/hospital/static/css/main.9a78b475.chunk.css"
+  },
+  {
+    "revision": "471a4b5c374ecc21aff4",
+    "url": "/hospital/static/js/1.471a4b5c.chunk.js"
+  },
+  {
+    "revision": "5bab7d4331d96ccb8a1f",
+    "url": "/hospital/static/js/runtime~main.5bab7d43.js"
+  },
+  {
+    "revision": "532751ebe3d5403af6b16fbabda3b170",
+    "url": "/hospital/static/media/W_amir_p30download.com.532751eb.ttf"
+  },
+  {
+    "revision": "e7a9ec1589d034afab830d56327b5afc",
+    "url": "/hospital/static/media/IRElham_p30download.com.e7a9ec15.ttf"
+  },
+  {
+    "revision": "2b4d64d2e4f1a60ed1f6",
+    "url": "/hospital/static/js/main.2b4d64d2.chunk.js"
+  },
+  {
+    "revision": "d237de656d82b8c9eda4779dbb2450f7",
+    "url": "/hospital/static/media/IRYekanBold_p30download.com.d237de65.ttf"
+  },
+  {
+    "revision": "77147e2303523ae1800efb59d6347066",
+    "url": "/hospital/static/media/IRYekan_p30download.com.77147e23.ttf"
+  },
+  {
+    "revision": "4a0e480ba8f4f95fc86102627f422c93",
+    "url": "/hospital/static/media/IRAmir_p30download.com.4a0e480b.ttf"
+  },
+  {
+    "revision": "4c6809daca141a4577659d558bd57b31",
+    "url": "/hospital/static/media/IRTerafikBold_p30download.com.4c6809da.ttf"
+  },
+  {
+    "revision": "c38e298ea90c9204bb879c175bc20bc3",
+    "url": "/hospital/index.html"
+  }
+];
